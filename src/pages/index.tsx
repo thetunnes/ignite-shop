1a8580c464466319ad09a315987c7139494369d7
Tunnes <cauepani06@gmail.com>
2022-11-15 15:45:22 -0300

    Create Project NextJS using TypeScript and Stitches

---
+++ src/pages/index.tsx
@@ -0,0 +1,20 @@
+import { styled } from "../styles"
+
+const Button = styled("button", {
+  backgroundColor: "$rocketseat",
+  borderRadius: 8,
+  border: 0,
+  padding: '4px 8px',
+
+  cursor: 'pointer',
+
+  '&:hover': {
+    filter: 'brightness(0.8)'
+  }
+ })
+
+export default function Home() {
+  return (
+    <Button>Enviar</Button>
+  )
+}
---
+++ src/pages/success.tsx
@@ -0,0 +1,9 @@
+
+
+
+export default function Success() {
+
+  return (
+    <h1>Success</h1>
+  )
+}
